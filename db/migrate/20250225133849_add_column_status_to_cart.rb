a2e186cd165f75893aea0e55df44cc283470f941
= <karl.xavier@austcommunitymedia.com.au>
2025-02-25 22:46:25 +0800

    refactor cart migration

--- db/migrate/20250225133849_add_column_status_to_cart.rb
+++ db/migrate/20250225133849_add_column_status_to_cart.rb
@@ -1,5 +1,5 @@
 class AddColumnStatusToCart < ActiveRecord::Migration[8.0]
   def change
-    add_column :carts, :status, :string
+    column_exists?(:carts, :status) || add_column(:carts, :status, :string)
   end
 end
